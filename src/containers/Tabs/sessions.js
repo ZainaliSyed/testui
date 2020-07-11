b8b0147d640d404c46922c7cd87fb98c2c0340f5
Shafique Abbasi <shafiqanwarabbasi@gmail.com>
2020-07-11 14:24:34 -0700

    sessions Screen complete ui

--- src/containers/Tabs/sessions.js
+++ src/containers/Tabs/sessions.js
@@ -14,19 +14,17 @@ import {Separator} from '../../components';
 import {FlatListHandler} from '../../reuseableComponents';
 
 export default class Sessions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pullUp: 'Pull-up',
-      pushUp: 'Push-up',
-      plank: 'Plank',
-      sitUp: 'Sit-up',
-      catPose: 'Cat-pose',
-      squats: 'Squats',
-      pullUp: 'Pull-up',
-    };
-  }
-
+  state = {
+    data: [
+      'Pull-up',
+      'Push-up',
+      'Plank',
+      'Sit-up',
+      'Cat-pose',
+      'Squats',
+      'Pull-up',
+    ],
+  };
   componentDidMount() {}
 
   dateScheduleTime = () => {
@@ -48,16 +46,14 @@ export default class Sessions extends Component {
     );
   };
 
-  exercises = () => {
-    return <View></View>;
-  };
-
-  renderItem = () => {
+  renderItem = ({item, index}) => {
+    const {data} = this.state;
+    console.log('item', item);
     return (
       <View style={styles.childViewChild3}>
         <View style={styles.excrsesMainChild}>
           <View style={styles.excrsesChild}>
-            <Text style={styles.excrsesText}>Pull-up</Text>
+            <Text style={styles.excrsesText}>{item}</Text>
             <View style={styles.excrsesWorks}>
               <Text style={styles.excrsesText2}>Set 5</Text>
               <Text style={styles.excrsesText2}>Reps 10</Text>
@@ -65,32 +61,23 @@ export default class Sessions extends Component {
             </View>
           </View>
           <View style={styles.excrsesChild2}>
-            <View
-              style={{
-                borderColor: '#1cb281',
-                borderWidth: 1,
-                borderRadius: 20,
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: 30,
-                height: 30,
-              }}>
-              <Image
-                source={Images.selectionTick}
-                style={{tintColor: '#1cb281'}}
-              />
-            </View>
+            {data.length - 1 == index ? (
+              <View style={styles.selectionDashView}></View>
+            ) : (
+              <View style={styles.selectionTickView}>
+                <Image
+                  source={Images.selectionTick}
+                  style={{tintColor: '#1CB281'}}
+                />
+              </View>
+            )}
           </View>
         </View>
-        <View style={{backgroundColor: '#202332'}}>
+        <View style={styles.ViewSeparator}>
           <Separator.FlatListSeparator style={{marginHorizontal: 16}} />
         </View>
       </View>
     );
-  };
-
-  renderSeparator = () => {
-    return <Separator.FlatListSeparator style={{marginHorizontal: 16}} />;
   };
 
   render() {
@@ -106,24 +93,26 @@ export default class Sessions extends Component {
             <Text style={styles.advns}>Advansed</Text>
           </View>
         </ImageBackground>
-        <View style={styles.childView2}>
-          {this.dateScheduleTime()}
+        <View style={{flex: 1}}>
+          <View style={styles.childView2}>
+            {this.dateScheduleTime()}
 
-          <View style={styles.childViewChild2}>
-            <View>
-              <Text style={styles.excrses}>Exercises</Text>
+            <View style={styles.childViewChild2}>
+              <View>
+                <Text style={styles.excrses}>Exercises</Text>
+              </View>
             </View>
+            <FlatListHandler
+              data={this.state.data}
+              style={{marginBottom: Metrics.heightRatio(60)}}
+              // fetchRequest={this.fetchScheduleList}
+              renderItem={this.renderItem}
+              // ItemSeparatorComponent={this.renderSeparator}
+              // emptyImage={Images.emptyNotification}
+              // isFetching={this.props.inAppNotificationsIsFetching}
+              showsHorizontalScrollIndicator={false}
+            />
           </View>
-          <FlatListHandler
-            data={['a', 'b', 'c', 'd', 'e']}
-            // style={{marginTop: Metrics.heightRatio(25)}}
-            // fetchRequest={this.fetchScheduleList}
-            renderItem={this.renderItem}
-            // ItemSeparatorComponent={this.renderSeparator}
-            // emptyImage={Images.emptyNotification}
-            // isFetching={this.props.inAppNotificationsIsFetching}
-            showsHorizontalScrollIndicator={false}
-          />
         </View>
       </View>
     );
@@ -136,7 +125,8 @@ const styles = StyleSheet.create({
   },
 
   Sessionbg: {
-    // width: '100%',
+    height: 240,
+    flex: 1,
   },
 
   childView: {
@@ -153,19 +143,22 @@ const styles = StyleSheet.create({
   workOut: {
     color: Colors.text.white,
     fontSize: Metrics.generatedFontSize(24),
+    fontWeight: 'bold',
     marginBottom: 10,
   },
 
   advns: {
     color: Colors.text.white,
     fontSize: Metrics.generatedFontSize(16),
-    marginBottom: 41,
+    marginBottom: 64,
   },
 
   childView2: {
     backgroundColor: '#5c746a',
     borderTopRightRadius: 30,
     borderTopLeftRadius: 30,
+
+    top: -100,
   },
 
   childViewChild: {
@@ -201,9 +194,13 @@ const styles = StyleSheet.create({
     color: 'white',
     marginTop: 10,
     marginBottom: 10,
+    fontWeight: 'bold',
+    fontSize: Metrics.generatedFontSize(18),
   },
   dateText2: {
     color: Colors.text.white,
+    fontSize: Metrics.generatedFontSize(14),
+    textAlign: 'center',
   },
 
   childViewChild2: {
@@ -217,14 +214,14 @@ const styles = StyleSheet.create({
 
   excrses: {
     color: Colors.text.white,
-    fontSize: Metrics.generatedFontSize(24),
+    fontSize: Metrics.generatedFontSize(20),
+    fontWeight: 'bold',
     marginTop: 35,
     marginBottom: 10,
   },
 
   childViewChild3: {
     backgroundColor: '#202332',
-    // backgroundColor: 'yellow',
   },
 
   excrsesMainChild: {
@@ -244,13 +241,15 @@ const styles = StyleSheet.create({
 
   excrsesText: {
     color: Colors.text.white,
-    fontSize: Metrics.generatedFontSize(24),
+    fontSize: Metrics.generatedFontSize(18),
+    fontWeight: 'bold',
     marginTop: 15,
   },
 
   excrsesText2: {
-    color: Colors.text.white,
-    fontSize: Metrics.generatedFontSize(16),
+    // color: Colors.text.white,
+    color: '#FFFFFF69',
+    fontSize: Metrics.generatedFontSize(12),
     marginTop: 5,
     marginBottom: 5,
   },
@@ -261,5 +260,27 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     justifyContent: 'center',
     marginRight: 10,
+  },
+
+  selectionTickView: {
+    borderColor: '#1cb281',
+    borderWidth: 2,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 36,
+    height: 36,
+  },
+
+  selectionDashView: {
+    borderColor: '#1cb281',
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 36,
+  },
+
+  ViewSeparator: {
+    backgroundColor: '#202332',
   },
 });
